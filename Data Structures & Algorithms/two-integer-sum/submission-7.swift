0e293c6f7b0943f9cb022c757353380774b8e3ed
orbitcoder <13703994+orbitcoder@users.noreply.github.com>
2026-08-27 14:35:41 +0530

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        for i in 0..<max(0, nums.count - 1) {
+            var reqVal = target - nums[i]
+          for j in (i + 1)..<max(0, nums.count) {
+                if (nums[j]  == reqVal ){
+                    return [i,j]
+                }
+            }  
+        }
+    return []
+    }
+
+}
